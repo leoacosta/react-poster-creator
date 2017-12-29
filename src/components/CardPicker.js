98e50d50c9fcc60a014c259bbbdaec7381eae077
leoacosta <hello@iamleoacosta.com>
2017-12-29 22:35:56 +1100

    Base form structure

--- src/components/CardPicker.js
+++ src/components/CardPicker.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 
 class CardPicker extends Component {
     render() {
-        return <p>Hello</p>
+        return (
+            <form className="card-picker">
+                <h2>Please enter your card title</h2>
+                <input type="text" placeholder="Card Name" required />
+                <button type="submit">View Card</button>
+            </form>
+        )
     }
 }
 
