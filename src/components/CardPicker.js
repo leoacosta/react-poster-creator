44f86017958fe2dfc67a39fe67c69cff36b2abf4
leoacosta <hello@iamleoacosta.com>
2017-12-30 12:05:49 +1100

    Updated markup structure

--- src/components/CardPicker.js
+++ src/components/CardPicker.js
@@ -4,8 +4,9 @@ class CardPicker extends Component {
     render() {
         return (
             <form className="card-picker">
-                <h2>Please enter your card title</h2>
-                <input type="text" placeholder="Card Name" required />
+                <h2 className="card-picker__title">Please enter your card title</h2>
+                <label className="card-picker__label" htmlFor="card-title">Create and customize your very own card component.</label>
+                <input type="text" id="card-title" placeholder="Card Title" required />
                 <button type="submit">View Card</button>
             </form>
         )
