b705654805d0cbf64b9fe9db905777e7d9479398
leoacosta <hello@iamleoacosta.com>
2017-12-30 16:02:59 +1100

    Refactored to be a stateless functional component

---
+++ src/components/CardTitle.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const CardTitle = (props) => {
+    return (
+        <header>
+            <h3 className="card-category">{props.cardCat}</h3>
+            <h2 className="card-title">{props.cardTitle}</h2>
+        </header>
+    )
+}
+
+export default CardTitle;
