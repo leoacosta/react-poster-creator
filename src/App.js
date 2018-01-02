be39f496cf5e411368e1b0eb92beff260aaa4df8
leoacosta <hello@iamleoacosta.com>
2018-01-02 21:17:43 +1100

    Spring cleaning...

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import CardTitle from './components/CardTitle'
-//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,13 +7,6 @@ class App extends Component {
     return (
       <div className="App">
         <CardTitle cardCat="Tango" cardTitle="Buenos Aires" />
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p> */}
       </div>
     );
   }
