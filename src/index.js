563996c1f6177982b443bd5eac393716a6c6a645
leoacosta <hello@iamleoacosta.com>
2018-10-12 14:40:41 +1100

    Remove router

--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './index.css';
 import App from './App';
-import PosterPicker from './components/PosterPicker';
-import NotFound from './components/NotFound';
-import registerServiceWorker from './registerServiceWorker';
 
-
-const Root = () => {
-    return (
-        <Router>
-            <Switch>
-                <Route exact path="/" component={PosterPicker} />
-                <Route path="/card/:cardId" component={App} />
-                <Route component={NotFound} />
-            </Switch>
-        </Router>
-    )
-}
-
-ReactDOM.render(<Root />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'));
